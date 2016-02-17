c4ef8d9ac0ebf6fd5fd3e46a5e24add95b24676c
Folkert Huizinga <folkerthuizinga@gmail.com>
2016-02-17 15:39:15 +0000

    Memleak on more realistic example

--- src/main.cpp
+++ src/main.cpp
@@ -1,62 +1,33 @@
-#include <iostream>
-#include <vector>
-
+#include <stdio.h>
 #include <sys/types.h>
 #include <unistd.h>
 #include <stdlib.h>
 
-#include <cuda_runtime_api.h>
-
-#define cudaSafeCall(expr) gpu::___cudaSafeCall(expr, __FILE__, __LINE__, __func__)
-
-namespace gpu
-{
-void error(const char *error_string, const char *file, const int line, const char *func = "");
-
-static inline void ___cudaSafeCall(cudaError_t err, const char *file, const int line, const char *func = "")
-{
-    if (cudaSuccess != err)
-        gpu::error(cudaGetErrorString(err), file, line, func);
-}
-
-void error(const char *error_string, const char *file, const int line, const char *func)
-{
-  std::cerr << "\n\nCudaError: " << error_string << std::endl;
-  std::cerr << "  File:  " << file << std::endl;
-  std::cerr << "  Line:  " << line << std::endl;
-  std::cerr << "  Func:  " << func << std::endl;
-  exit(1);
-}
-
-int memOp(int *data, int n)
-{
-  int *d_data;
-
-  size_t data_size = n * sizeof(int);
-  cudaSafeCall(cudaMalloc((void**)&d_data, data_size));
-
-  cudaSafeCall(cudaMemcpy(d_data, data, data_size, cudaMemcpyHostToDevice));
-  cudaSafeCall(cudaDeviceSynchronize());
-
-  for (int i = 0; i < 1000000; i++)
-  {
-    cudaSafeCall(cudaMemcpy(data, d_data, data_size, cudaMemcpyDeviceToHost));
-    cudaSafeCall(cudaDeviceSynchronize());
-  }
-
-  cudaSafeCall(cudaFree(d_data));
-  cudaSafeCall(cudaDeviceSynchronize());
-
-  return 0;
-}
-}
+#include "transform.h"
 
 using namespace std;
 
-int main(int argc, char *argv[])
+int main(void)
 {
-  std::cout << "pid: " << getpid() << std::endl << std::endl;
-  vector<int> data(512,0);
-  gpu::memOp(data.data(), data.size());
+  printf("pid: %i\n", getpid());
+
+  int w = 64;
+  int h = 64;
+  float img[w*h];
+  float res[w*h];
+
+  for (int i = 0; i < w*h; i++)
+    img[i] = 1.0f;
+
+  int N = 10000000;
+  for (int i = 0; i < N; i++)
+    process(img, res, w, h, i);
+
+  for (int i = 0; i < h; i++)
+  {
+    for (int j = 0; j < w; j++)
+      printf("%i ", int(res[i*w+j]));
+    printf("\n");
+  }
   return 0;
 }
